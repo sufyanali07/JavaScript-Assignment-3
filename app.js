c6cdc7fa54c6107635ea5397560083d2e81544bb
sufyanali07 <sufyan.ali6800@gmail.com>
2022-11-10 00:27:09 +0500

    Files Added

---
+++ app.js
@@ -0,0 +1,114 @@
+//===============================
+
+var a = ["a","b","c","d","e"]
+
+//===============================
+
+
+//a.push("z");
+//a.pop();
+//a.shift();
+//a.unshift("x");
+//*var b = a.slice(1,3);
+//var b = a.splice(2,3);
+
+
+//console.log(a); 
+//console.log(b); (jismy var b hy us mein lagega)
+
+
+//===============================
+
+
+//a.splice(2 , 1 ,"x","y","z")
+
+//console.log(a);
+
+
+//===============================
+
+
+//var i = a.indexOf("d");
+
+//console.log(a[i]);
+
+
+//===============================
+
+
+//var b = a.length;
+
+//console.log(b);
+
+
+//===============================
+
+//for ( var i = 0 ; i < 10 ; i++ ) {
+
+//    alert("abc");
+//    console.log(i)
+
+//}
+
+//===============================
+
+//======TABLE=======//
+
+//for ( var i = 0 ; i < 10 ; i++ ) {
+   //console.log(" 12 X " + (i + 1 ) + " = " + 12 * (i + 1) )   
+//}
+
+//==============================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // program to generate a multiplication table
+
+// // take input from the user
+// const number = parseInt(prompt('Enter an integer: '));
+
+// //creating a multiplication table
+// for(let i = 1; i <= 10; i++) {
+
+//     // multiply i with number
+//     const result = i * number;
+
+//     // display the result
+//     console.log(`${number} * ${i} = ${result}`);
+// }
